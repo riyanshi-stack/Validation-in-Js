6a2ec1f6267ad6c7c178d013ae68156f9e90c0cb
riyanshi81 <nidhi.sinha81@gmail.com>
2025-04-11 19:15:30 +0530

    Intial

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //Clears the Calculator display
-function clearScreen()
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+//Appends the clicked button's value to the display
+function setScreenValue() {
+    document.getElementById(value)
+}
